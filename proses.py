38a37073e6304e87de5d0800cfc5dfc30940d299
Ammar Rozan Annafi <ammar.rozan.2401@gmail.com>
2025-06-24 10:52:28 +0700

    Add files via upload

---
+++ proses.py
@@ -0,0 +1,15 @@
+class Pengadonan:
+    def pengadon(self):
+        print(f"{self.nama} : Pengadonan dimulai.")
+
+class Pengembangan:
+    def pengembang(self):
+        print(f"{self.nama} : Melakukan pengembangan.")
+
+class Pemanggangan:
+    def pemanggang(self):
+        print(f"{self.nama} : Memanggang.")
+
+class Topping:
+    def pemberi_topping(self):
+        print(f"{self.nama} : Memberi topping.")
